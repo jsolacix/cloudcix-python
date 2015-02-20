f173f375c169f30e58d2c200219beedc2780eeff
Lukasz Baczynski <contact@lukaszbaczynski.com>
2015-02-20 17:03:39 +0100

    Python SDK prototype

---
+++ tests.py
@@ -0,0 +1,100 @@
+# python
+from __future__ import unicode_literals
+import os
+import sys
+import unittest
+
+# libs
+from keystoneclient.exceptions import AuthorizationFailure
+
+# test imports
+
+ROOT = lambda base: os.path.abspath(os.path.join(
+    os.path.dirname(__file__), base).replace('\\', '/'))
+sys.path.insert(0, ROOT('../'))
+
+os.environ['CLOUDCIX_SETTINGS_MODULE'] = 'cloudcix_sdk.settings_local'
+from cloudcix_sdk import api
+from cloudcix_sdk.utils import (get_admin_session, get_admin_client, settings,
+    KeystoneSession, KeystoneClient, KeystoneTokenAuth)
+from cloudcix_sdk.cloudcixauth import CloudCIXAuth
+from keystoneclient.exceptions import NotFound
+
+
+class TestAPIUseCases(unittest.TestCase):
+
+    def setUp(self):
+        self.assertTrue(settings.CLOUDCIX_SERVER_URL, 'Test cases expect that '
+                        'you create settings_local.py with required values.'
+                        'See README for list of required OPENSTACK and '
+                        'CLOUDCIX variables.')
+
+    def test_basic_use_case(self):
+        """basic_use_case
+
+        1) Get an admin token
+        2) read the language service
+        """
+        token = get_admin_session().get_token()
+        self.assertTrue(token)
+        response = api.membership.language.list(token=token)
+        self.assertEqual(response.status_code, 200)
+        self.assertGreater(len(response.json()['content']), 0)
+
+    def test_user_token(self):
+        """
+        1) Get a token for a user
+        2) using admin session ensure the token is valid
+        3) read user's address
+        4) dispose of the token"""
+        # create auth payload
+        auth = CloudCIXAuth(
+            auth_url=settings.OPENSTACK_KEYSTONE_URL,
+            username=settings.CLOUDCIX_API_USERNAME,
+            password=settings.CLOUDCIX_API_PASSWORD)
+        auth_session = KeystoneSession(auth=auth)
+        user_token = auth_session.get_token()
+        token_data = auth_session.auth.auth_ref
+
+        # for the sake of example check that the token is valid
+        # you should use your admin credentials to check user's token
+        admin_cli = get_admin_client()
+        try:
+            admin_cli.tokens.validate(user_token)
+        except NotFound as e:
+            # Token is invalid, re-raise the exception
+            raise e
+        # token is valid, continue
+
+        # Finally, read the address
+        response = api.membership.address.read(
+            pk=token_data['user']['address']['idAddress'],
+            token=user_token)
+
+        # check the response status to ensure we've been able to read the
+        # address
+        self.assertEqual(response.status_code, 200)
+
+        # check the object
+        self.assertTrue(response.json()['content'])
+
+        # Finally delete the token
+        admin_cli.tokens.revoke_token(user_token)
+        # And make sure it's not longer valid
+        try:
+            admin_cli.tokens.validate(user_token)
+        except NotFound as e:
+            # Token is invalid pass the test
+            pass
+
+    def test_extend_token(self):
+        token = get_admin_session().get_token()
+        auth = KeystoneTokenAuth(
+            auth_url=settings.OPENSTACK_KEYSTONE_URL,
+            token=token)
+        session = KeystoneSession(auth=auth)
+        self.assertNotIn(session.get_token(), [token, '', None])
+        self.assertTrue(session.auth.auth_ref)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ utils.py
@@ -0,0 +1,72 @@
+# python
+from __future__ import unicode_literals
+import importlib
+import os
+
+# libs
+from keystoneclient.session import Session as KeystoneSession
+from keystoneclient.v3.client import Client as KeystoneClient
+from keystoneclient.auth.identity.v3 import Token as KeystoneTokenAuth
+
+# local
+from .cloudcixauth import CloudCIXAuth
+
+__all__ = ['KeystoneSession', 'KeystoneClient', 'KeystoneTokenAuth',
+           'settings', 'get_admin_session', 'get_admin_client']
+
+
+def new_method_proxy(func):
+    """When attribute is accessed in lazy object, this method makes sure that
+    lazy object was properly initialized and _setup method has been run
+    """
+    def inner(self, *args):
+        if self._wrapped is None:
+            self._setup()
+        return func(self._wrapped, *args)
+    return inner
+
+
+class LazySettings(object):
+    """Lazy settings module. We want settings to be imported when they are
+    accessed not earlier.
+    """
+    _wrapped = None
+    __getattr__ = new_method_proxy(getattr)
+
+    def _setup(self):
+        """
+        Load the settings module pointed to by the environment variable. This
+        is used the first time we need any settings at all, if the user has not
+        previously configured the settings manually.
+        """
+        try:
+            settings_module = os.environ['CLOUDCIX_SETTINGS_MODULE']
+            if not settings_module:  # If it's set but is an empty string.
+                raise KeyError
+        except KeyError:
+            raise ImportError("You must specify the CLOUDCIX_SETTINGS_MODULE "
+                              "environment variable.")
+        else:
+            self._wrapped = importlib.import_module(settings_module)
+
+
+settings = LazySettings()
+
+
+def get_admin_session():
+    t = CloudCIXAuth(
+        auth_url=settings.OPENSTACK_KEYSTONE_URL,
+        username=settings.CLOUDCIX_API_USERNAME,
+        password=settings.CLOUDCIX_API_PASSWORD,
+        idMember=settings.CLOUDCIX_API_ID_MEMBER,
+        scope={'domain': {'id': settings.CLOUDCIX_API_ID_MEMBER}})
+    admin_session = KeystoneSession(auth=t)
+    admin_session.get_token()
+    return admin_session
+
+
+def get_admin_client():
+    admin_session = get_admin_session()
+    return KeystoneClient(session=admin_session,
+                          auth_url=settings.OPENSTACK_KEYSTONE_URL,
+                          endpoint_override=settings.OPENSTACK_KEYSTONE_URL)
